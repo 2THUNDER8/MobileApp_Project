db3120c894a5f56cc82a83a4d8f501586d4ea042
THUNDERapd <mr.alizdhital2nd8@gmail.com>
2022-03-14 14:13:06 -0300

    js for phase 1

--- phase1.js
+++ phase1.js
@@ -4,13 +4,17 @@ var isVis = true;
 
 function vis() {
     if (isVis) {
-        document.getElementById("CH2").style.display = "none";
-        document.getElementById("CH3").style.display = "none";
+        document.getElementById("CH2").style.visibility = "hidden";
+        document.getElementById("CH3").style.visibility = "hidden";
+        document.getElementById("keyB").style.visibility = "hidden";
+        document.getElementById("inpBlock").style.visibility = "hidden";
         isVis = false;
     }
     else {
-        document.getElementById("CH2").style.display = "block";
-        document.getElementById("CH3").style.display = "block";
+        document.getElementById("CH2").style.visibility = "visible";
+        document.getElementById("CH3").style.visibility = "visible";
+        document.getElementById("keyB").style.visibility = "visible";
+        document.getElementById("inpBlock").style.visibility = "visible";
         isVis = true;
     }
 }
