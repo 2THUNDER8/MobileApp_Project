694d5980b54fc3b7b524e6e4fc1d2143bf1cbfdf
THUNDERapd <mr.alizdhital2nd8@gmail.com>
2022-03-15 16:38:33 -0300

    Small changes

--- phase1.js
+++ phase1.js
@@ -4,15 +4,15 @@ var isVis = true;
 
 function vis() {
     if (isVis) {
-        document.getElementById("CH2").style.visibility = "hidden";
-        document.getElementById("CH3").style.visibility = "hidden";
+        document.getElementById("CH2").style.display = "none";
+        document.getElementById("CH3").style.display = "none";
         document.getElementById("keyB").style.visibility = "hidden";
         document.getElementById("inpBlock").style.visibility = "hidden";
         isVis = false;
     }
     else {
-        document.getElementById("CH2").style.visibility = "visible";
-        document.getElementById("CH3").style.visibility = "visible";
+        document.getElementById("CH2").style.display = "block";
+        document.getElementById("CH3").style.display = "block";
         document.getElementById("keyB").style.visibility = "visible";
         document.getElementById("inpBlock").style.visibility = "visible";
         isVis = true;
